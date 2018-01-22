95d7fe61d25597d5533a8b9851c6ddbf34412fc6
Necdet Can Ateşman <can.atesman@strg.at>
2018-01-22 16:49:09 +0100

    Release: 0.0.5

--- score/requirejs/__init__.py
+++ score/requirejs/__init__.py
@@ -1,4 +1,4 @@
-# Copyright © 2017 STRG.AT GmbH, Vienna, Austria
+# Copyright © 2017,2018 STRG.AT GmbH, Vienna, Austria
 #
 # This file is part of the The SCORE Framework.
 #
@@ -26,6 +26,6 @@
 
 from ._init import init, ConfiguredRequirejsModule
 
-__version__ = '0.0.4'
+__version__ = '0.0.5'
 
 __all__ = ('init', 'ConfiguredRequirejsModule')
